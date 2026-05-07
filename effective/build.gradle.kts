022bb3e845d0be00550b8d2e4c8e65176078df5a
Unitei <136928305+OneWorldStudio@users.noreply.github.com>
2026-05-07 21:50:19 +0300

    Resolve Effective Gradle conflict

--- effective/build.gradle.kts
+++ effective/build.gradle.kts
@@ -1,14 +1,17 @@
+import net.fabricmc.loom.api.LoomGradleExtensionAPI
+
 val effectiveModId: String by project
 val effectiveModVersion: String by rootProject
+val effectiveModDescription: String by project
 val effectiveMavenGroup: String by rootProject
 val minecraftVersion: String by rootProject
 
 plugins {
-    kotlin("jvm")
-    id("io.github.pacifistmc.forgix")
-    id("architectury-plugin")
-    id("dev.architectury.loom-no-remap")
-    id("com.gradleup.shadow") apply false
+    kotlin("jvm") version "2.2.21"
+    id("io.github.pacifistmc.forgix") version "1.2.9"
+    id("architectury-plugin") version "3.5-SNAPSHOT"
+    id("dev.architectury.loom-no-remap") version "1.14-SNAPSHOT" apply false
+    id("com.gradleup.shadow") version "8.3.6" apply false
 }
 
 architectury {
@@ -21,9 +24,20 @@ forgix {
     outputDir = "build"
 }
 
+allprojects {
+    group = effectiveMavenGroup
+    version = effectiveModVersion
+    description = effectiveModDescription
+}
+
 subprojects {
     apply(plugin = "kotlin")
     apply(plugin = "architectury-plugin")
+    apply(plugin = "dev.architectury.loom-no-remap")
+
+    val loom = project.extensions.getByName<LoomGradleExtensionAPI>("loom").apply {
+        silentMojangMappingsLicense()
+    }
 
     java {
         toolchain {
@@ -51,6 +65,7 @@ subprojects {
 
     dependencies {
         "minecraft"("com.mojang:minecraft:$minecraftVersion")
+        @Suppress("UnstableApiUsage") "mappings"(loom.officialMojangMappings())
     }
 
 }
