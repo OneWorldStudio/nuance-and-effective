01b467319b6dd37f8fbe29a3834b90107eca6255
Unitei <136928305+OneWorldStudio@users.noreply.github.com>
2026-05-07 21:40:55 +0300

    Apply Loom before common dependencies

--- effective/build.gradle.kts
+++ effective/build.gradle.kts
@@ -1,17 +1,14 @@
-import net.fabricmc.loom.api.LoomGradleExtensionAPI
-
 val effectiveModId: String by project
 val effectiveModVersion: String by rootProject
-val effectiveModDescription: String by project
 val effectiveMavenGroup: String by rootProject
 val minecraftVersion: String by rootProject
 
 plugins {
-    kotlin("jvm") version "2.2.21"
-    id("io.github.pacifistmc.forgix") version "1.2.9"
-    id("architectury-plugin") version "3.5-SNAPSHOT"
-    id("dev.architectury.loom-no-remap") version "1.14-SNAPSHOT" apply false
-    id("com.gradleup.shadow") version "8.3.6" apply false
+    kotlin("jvm")
+    id("io.github.pacifistmc.forgix")
+    id("architectury-plugin")
+    id("dev.architectury.loom-no-remap")
+    id("com.gradleup.shadow") apply false
 }
 
 architectury {
@@ -24,19 +21,18 @@ forgix {
     outputDir = "build"
 }
 
-allprojects {
-    group = effectiveMavenGroup
-    version = effectiveModVersion
-    description = effectiveModDescription
-}
-
 subprojects {
     apply(plugin = "kotlin")
     apply(plugin = "architectury-plugin")
-    apply(plugin = "dev.architectury.loom-no-remap")
 
-    val loom = project.extensions.getByName<LoomGradleExtensionAPI>("loom").apply {
-        silentMojangMappingsLicense()
+    java {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(25))
+        }
+    }
+
+    kotlin {
+        jvmToolchain(25)
     }
 
     base {
@@ -44,6 +40,7 @@ subprojects {
     }
 
     repositories {
+        mavenCentral()
         maven("https://maven.parchmentmc.org")
         maven("https://maven.terraformersmc.com/")
         maven("https://raw.githubusercontent.com/Fuzss/modresources/main/maven/")
@@ -54,7 +51,6 @@ subprojects {
 
     dependencies {
         "minecraft"("com.mojang:minecraft:$minecraftVersion")
-        @Suppress("UnstableApiUsage") "mappings"(loom.officialMojangMappings())
     }
 
 }
